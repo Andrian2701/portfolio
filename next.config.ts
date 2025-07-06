cdbe97e7cd8e3c1a0898448ad9a503a64012f68e
Andrian Smetaniuk <andrian.smet@gmail.com>
2025-07-06 23:31:21 +0300

    fix: client side error caused by react-pdf 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  swcMinify: false,
+};
 
 const withNextIntl = createNextIntlPlugin();
 
