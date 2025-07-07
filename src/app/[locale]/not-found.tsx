9f85e8f0f1dc8d13eac52c3de51982e14e37643b
Andrian Smetaniuk <andrian.smet@gmail.com>
2025-07-07 12:28:54 +0300

    update: updated not-found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -7,14 +7,16 @@ const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center flex-auto">
       <div className="flex flex-col gap-8">
-        <h1 className="text-primary text-center font-bold text-[40px] sm:text-[45px] tracking-wide">
+        <h1 className="text-text-hover text-center font-bold text-[40px] sm:text-[45px] tracking-wide">
           404
         </h1>
         <div className="flex flex-col items-center gap-4">
-          <p className="font-light text-primary">{t('notFoundTitle')}</p>
+          <p className="font-light text-base text-text-primary">
+            {t('notFoundTitle')}
+          </p>
           <Link
             href="/"
-            className="hover:underline font-light transition delay-150 duration-300 ease-in-out text-primary cursor-pointer"
+            className="underline font-light text-text-hover hover:text-indigo-500 text-base"
           >
             {t('redirectHome')}
           </Link>
